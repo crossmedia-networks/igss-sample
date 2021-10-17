2819cafcf974501a638e2431e2f6478e65408b6e
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2021-10-18 04:33:30 +0900

    グラフ表示できるようにした

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const URL = process.env.URL
+//var http = require('http');
+var request = require('request');
 
-console.log(URL)
 
-express()
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/view', function(req, res){
+    // APIを呼び出す
+    var options = {
+	url: `${URL}/sensor/demo/${req.body.lpwa_id}/tmp?start=${req.body.start}&interval=${req.body.interval}`,
+	method: 'GET',
+	json: true
+    }
+    request(options, function (error, response, body) {
+	label = body.map((x) => {return Date.parse(x.time) })
+	value = body.map((x) => {return x.value})
+	res.render('view', {label: label, value: value })
+    })
+
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
